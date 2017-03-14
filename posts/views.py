4643d1da0b2d7b42bae4d204607c4c0e8b662d45
Yash Sharma <yasharmaster@gmail.com>
2017-03-14 18:52:52 +0530

    Add basic template & update posts urls

--- posts/views.py
+++ posts/views.py
@@ -3,5 +3,13 @@ from django.http import HttpResponse
 
 # Create your views here.
 
-def post_home(request):
-	return HttpResponse("<h1>Hello</h1>")
+def post_create(request):
+	return HttpResponse("<h1>Create</h1>")
+def post_detail(request):
+	return HttpResponse("<h1>Detail</h1>")
+def post_list(request):
+	return render(request, "index.html", {})
+def post_update(request):
+	return HttpResponse("<h1>Update</h1>")
+def post_delete(request):
+	return HttpResponse("<h1>Delete</h1>")
