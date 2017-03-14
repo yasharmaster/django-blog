a017f1a8ad7956fd503f35119aa3390610310975
Yash Sharma <yasharmaster@gmail.com>
2017-03-14 22:59:01 +0530

    Add create posts form

--- posts/views.py
+++ posts/views.py
@@ -1,15 +1,38 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
 from django.http import HttpResponse
 
 # Create your views here.
+from .models import Post
+from .forms import PostForm
 
 def post_create(request):
-	return HttpResponse("<h1>Create</h1>")
-def post_detail(request):
-	return HttpResponse("<h1>Detail</h1>")
+	form = PostForm(request.POST or None)
+	if form.is_valid():
+		instance = form.save(commit=False)
+		instance.save()
+	context = {
+		"form": form,
+	}
+	return render(request, "post_form.html", context)
+
+def post_detail(request, id=None):
+	instance = get_object_or_404(Post, id=id)
+	context = {
+		"title": instance.title,
+		"instance": instance,
+	}
+	return render(request, "post_detail.html", context)
+
 def post_list(request):
-	return render(request, "index.html", {})
+	queryset = Post.objects.all()
+	context = {
+		"object_list": queryset,
+		"title": "List"
+	}
+	return render(request, "index.html", context)
+
 def post_update(request):
 	return HttpResponse("<h1>Update</h1>")
+
 def post_delete(request):
 	return HttpResponse("<h1>Delete</h1>")
