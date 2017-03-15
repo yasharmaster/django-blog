4c5f7d9bdc337ccd53973f6e97615c7cfd02925c
Yash Sharma <yasharmaster@gmail.com>
2017-03-15 12:12:09 +0530

    Add image field to posts

--- posts/views.py
+++ posts/views.py
@@ -1,13 +1,14 @@
 from django.shortcuts import render, get_object_or_404, redirect
 from django.contrib import messages
 from django.http import HttpResponse, HttpResponseRedirect
+from django.core.paginator import Paginator, EmptyPage, PageNotAnInteger
 
 # Create your views here.
 from .models import Post
 from .forms import PostForm
 
 def post_create(request):
-	form = PostForm(request.POST or None)
+	form = PostForm(request.POST or None, request.FILES or None)
 	if form.is_valid():
 		instance = form.save(commit=False)
 		instance.save()
@@ -29,16 +30,29 @@ def post_detail(request, id=None):
 	return render(request, "post_detail.html", context)
 
 def post_list(request):
-	queryset = Post.objects.all()
+	queryset_list = Post.objects.all().order_by("-timestamp")
+	paginator = Paginator(queryset_list, 2) # Show 2 blogs per page
+
+	page_request_var = 'page'
+	page = request.GET.get(page_request_var)
+	try:
+		queryset = paginator.page(page)
+	except PageNotAnInteger:
+		# If page is not an integer, deliver first page.
+		queryset = paginator.page(1)
+	except EmptyPage:
+		# If page is out of range (e.g. 9999), deliver last page of results.
+		queryset = paginator.page(paginator.num_pages)
 	context = {
 		"object_list": queryset,
-		"title": "List"
+		"title": "List",
+		"page_request_var": page_request_var
 	}
 	return render(request, "post_list.html", context)
 
 def post_update(request, id=None):
 	instance = get_object_or_404(Post, id=id)
-	form = PostForm(request.POST or None, instance=instance)
+	form = PostForm(request.POST or None, request.FILES or None, instance=instance)
 	if form.is_valid():
 		instance = form.save(commit=False)
 		instance.save()
